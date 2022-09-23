819cc84f9f59cb710ef22fd9b62dab38e84c925b
bignick1958 <bignick58@gmail.com>
2022-09-23 19:29:42 +0300

    Добавление окна результатов

--- abcalculator.py
+++ abcalculator.py
@@ -5,6 +5,13 @@ import tkinter as tk
 def do_close():
     root.destroy()
 
+def popup_window():
+    window = tk.Toplevel()
+    window.geometry('280x300')
+    window.title('А/В Результат')
+    btn_close_popup = tk.Button(window, text='Закрыть', font=('Bahnschrift', 12, 'bold'), command= window.destroy)
+    btn_close_popup.place(x=160, y=250, width=90, height=30)
+
 # главное окно программы
 root = tk.Tk()
 root.geometry('280x300')
@@ -16,7 +23,7 @@ lblTitle.place(x=55, y=20)
 
 # Creating hadler lable for control group
 lblTitle1 = tk.Label(text='Контрольная группа', font=('Bahnschrift', 13, 'bold'), fg='#0066ff')
-lblTitle1.place(x=45, y=50)
+lblTitle1.place(x=25, y=50)
 
 # Adding input labels for control group
 lblVisitors1= tk.Label(text='Посетители', font=('Bahnschrift', 10, 'bold'))
@@ -33,7 +40,7 @@ entonversions1.place(x=115, y=115, width=90, height=20)
 
 # Creating hadler lable for test group
 lblTitle2 = tk.Label(text='Тестовая группа', font=('Bahnschrift', 13, 'bold'), fg='#008800')
-lblTitle2.place(x=55, y=140)
+lblTitle2.place(x=25, y=140)
 
 # Adding input labels for test group
 lblVisitors2= tk.Label(text='Посетители', font=('Bahnschrift', 10, 'bold'))
@@ -49,7 +56,7 @@ entConversions2 = tk.Entry(font=('Bahnschrift', 10, 'bold'))
 entConversions2.place(x=115, y=205, width=90, height=20)
 
 # Creating knob to calculate
-btnProccess = tk.Button(text='Рассчитать', font=('Bahnschrift', 12, 'bold'))
+btnProccess = tk.Button(text='Рассчитать', font=('Bahnschrift', 12, 'bold'), command=popup_window)
 btnProccess.place(x=25, y=250, width=110, height=30)
 
 #3

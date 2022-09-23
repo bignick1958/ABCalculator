b3d9d51e883f690b77ba0a9b571c73c350c89009
bignick1958 <bignick58@gmail.com>
2022-09-23 07:28:21 +0300

    Создание программы А/В Калькулятор

---
+++ abcalculator.py
@@ -0,0 +1,11 @@
+#А/В калькулятор
+
+import tkinter as tk
+
+# главное окно программы
+root = tk.Tk()
+root.geometry('280x300')
+root.title('A/B калькулятор')
+
+# запуск цикла mainloop()
+root.mainloop()

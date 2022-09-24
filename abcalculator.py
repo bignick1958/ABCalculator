b052f71b6585b80d0c86f8c998acc4bab9d908eb
bignick1958 <bignick58@gmail.com>
2022-09-24 19:44:47 +0300

    Добавление оценки результатов

--- abcalculator.py
+++ abcalculator.py
@@ -10,7 +10,7 @@ def do_close():
     root.destroy()
 
 
-# num formatting funcction
+# num formatting function
 def num_percent(num):
     return "{:.2f}".format(num * 100).rjust(10) + '%'
 
@@ -38,22 +38,22 @@ def popup_window(n1, c1, n2, c2):
 
     # adding text output window
     txt_output = tk.Text(window, font=('Courier New', 10, 'bold'))
-    txt_output.place(x=15, y=65, width=470, height=370)
+    txt_output.place(x=15, y=105, width=470, height=330)
 
     # adding headler's
-    txt_output.insert(tk.END, '                          Контрольная      Тестовая' + os.linesep)
-    txt_output.insert(tk.END, '                          группа           группа  ' + os.linesep)
+    txt_output.insert(tk.END, '                            Контрольная      Тестовая' + os.linesep)
+    txt_output.insert(tk.END, '                            группа           группа  ' + os.linesep)
     txt_output.insert(tk.END, '__________________________________________________________' + os.linesep)
 
     # adding output for conversion and standart deviation
     p1 = c1 / n1
     p2 = c2 / n2
-    txt_output.insert(tk.END, 'Конверсия             ' + num_percent(p1)
+    txt_output.insert(tk.END, '  Конверсия             ' + num_percent(p1)
                       + '     ' + num_percent(p2) + os.linesep)
     sigma1 = math.sqrt(p1 * (1 - p1) / n1)
     sigma2 = math.sqrt(p2 * (1 - p2) / n2)
     txt_output['bg'] ='#C5C6C6'
-    txt_output.insert(tk.END, 'Стандартное отклонение' + num_percent(sigma1)
+    txt_output.insert(tk.END, '  Стандартное отклонение' + num_percent(sigma1)
                       + '     ' + num_percent(sigma2) + os.linesep)
     txt_output.insert(tk.END, '__________________________________________________________' + os.linesep)
 
@@ -72,11 +72,11 @@ def popup_window(n1, c1, n2, c2):
     upper2_95 = p2 + z1 * sigma2
     if upper2_95 > 1:
         upper2_95 = 1
-    print("          ")
-    txt_output.insert(tk.END, '95% Возможный разброс   ' + os.linesep)
-    txt_output.insert(tk.END, '                   от ' + num_percent(lower1_95)
+
+    txt_output.insert(tk.END, '  95% Возможный разброс   ' + os.linesep)
+    txt_output.insert(tk.END, '                   от   ' + num_percent(lower1_95)
                       + '     '+ num_percent(lower2_95)+ os.linesep)
-    txt_output.insert(tk.END, '                   До ' + num_percent(upper1_95)
+    txt_output.insert(tk.END, '                   До   ' + num_percent(upper1_95)
                       + "     " + num_percent(upper2_95) + os.linesep)
     txt_output.insert(tk.END, '__________________________________________________________' + os.linesep)
 
@@ -94,18 +94,48 @@ def popup_window(n1, c1, n2, c2):
     upper2_99 = p2 + z2 * sigma2
     if upper2_99 > 1:
         upper2_99 = 1
-    print("          ")
-    txt_output.insert(tk.END, '99% Возможный разброс   ' + os.linesep)
-    txt_output.insert(tk.END, '                   от ' + num_percent(lower1_99)
+
+    txt_output.insert(tk.END, '  99% Возможный разброс   ' + os.linesep)
+    txt_output.insert(tk.END, '                   от   ' + num_percent(lower1_99)
                       + '     ' + num_percent(lower2_99) + os.linesep)
-    txt_output.insert(tk.END, '                   До ' + num_percent(upper1_99)
+    txt_output.insert(tk.END, '                   До   ' + num_percent(upper1_99)
                       + "     " + num_percent(upper2_99) + os.linesep)
-    txt_output.insert(tk.END, '__________________________________________________________' + os.linesep + os.linesep)
+    txt_output.insert(tk.END, '__________________________________________________________' + os.linesep )
+
     # Z and P calculating
     z_score = (p2-p1)/math.sqrt(sigma1*sigma1 + sigma2*sigma2)
-    txt_output.insert(tk.END, 'Z = ' + '{:.7f}'.format(z_score)+ os.linesep)
+    txt_output.insert(tk.END, '  Z = ' + '{:.7f}'.format(z_score)+ os.linesep)
     p_value = norm.sf(x = z_score, loc = 0, scale= 1)
-    txt_output.insert(tk.END, 'p =  ' + "{:.7f}".format(p_value)+ os.linesep)
+    txt_output.insert(tk.END, '  P =  ' + "{:.7f}".format(p_value)+ os.linesep)
+
+    # result evaluation adding
+    confidence_95 = False
+    if p_value<0.025 or p_value > 0.975:
+        confidence_95 = True
+
+    confidence_99 = False
+    if p_value<0.005 or p_value > 0.995:
+        confidence_99 = True
+
+    lbl_comments95 = tk.Label(window, text= "95% уверенность:", font=('Helvetica', 10, 'bold'), bg='#8fffff')
+    lbl_comments95.place(x=25, y=25)
+
+    if confidence_95:
+        lblResult95 = tk.Label(window, text="Да", font=('Helvetica', 12, 'bold'), fg='#008800', bg='#8fffff')
+        lblResult95.place(x=160, y=25)
+    else:
+        lblResult95 = tk.Label(window, text="Нет", font=('Helvetica', 12, 'bold'), fg='#ff0000', bg = '#8fffff')
+        lblResult95.place(x=160, y=25)
+
+    lbl_comments99 = tk.Label(window, text="99% уверенность:", font=('Helvetica', 10, 'bold'), bg='#8fffff')
+    lbl_comments99.place(x=25, y=65)
+    if confidence_99:
+        lblResult99 = tk.Label(window, text="Да", font=('Helvetica', 12, 'bold'), fg='#008800', bg='#8fffff')
+        lblResult99.place(x=160, y=65)
+    else:
+        lblResult99 = tk.Label(window, text="Нет", font=('Helvetica', 12, 'bold'), fg='#ff0000', bg='#8fffff')
+        lblResult99.place(x=160, y=65)
+
 
 
     # widow closing knob
@@ -136,14 +166,14 @@ lblVisitors1.place(x=25, y=85)
 
 entVisitors1 = tk.Entry(font=('Bahnschrift', 10, 'bold'), bg='#C5C6C6', justify='center')
 entVisitors1.place(x=115, y=85, width=90, height=20)
-entVisitors1.insert(tk.END, '255')
+entVisitors1.insert(tk.END, '0')
 
 lblConversions1 = tk.Label(text='Конверсия', font=('Bahnschrift', 10, 'bold'), bg='#C5C6C6')
 lblConversions1.place(x=25, y=115)
 
 entConversions1 = tk.Entry(font=('Bahnschrift', 10, 'bold'), bg='#C5C6C6', justify='center')
 entConversions1.place(x=115, y=115, width=90, height=20)
-entConversions1.insert(tk.END, '26')
+entConversions1.insert(tk.END, '0')
 
 # Creating hadler lable for test group
 lblTitle2 = tk.Label(text='Тестовая группа', font=('Bahnschrift', 13, 'bold'), fg='#008800', bg='#C5C6C6')
@@ -155,7 +185,7 @@ lblVisitors2.place(x=25, y=175)
 
 entVisitors2 = tk.Entry(font=('Bahnschrift', 10, 'bold'), bg='#C5C6C6', justify='center')
 entVisitors2.place(x=115, y=175, width=90, height=20)
-entVisitors2.insert(tk.END, '235')
+entVisitors2.insert(tk.END, '0')
 
 lblConversions2 = tk.Label(text='Конверсия', font=('Bahnschrift', 10, 'bold'), bg='#C5C6C6')
 lblConversions2.place(x=25, y=205)
